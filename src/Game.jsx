a1c96c9fbf37bd079e00d701e97bc15d9dcffc02
Mark Wheeler <mhwheeler@gmail.com>
2024-04-16 13:18:02 -0700

    Style the solved groups

--- src/Game.jsx
+++ src/Game.jsx
@@ -7,18 +7,22 @@ const gameData = {
   groups: [
     {
       description: "Foods",
+      className: "easy",
       words: ["Taco", "Donut", "Popcorn", "Burrito"],
     },
     {
       description: "Utensils",
+      className: "medium",
       words: ["Fork", "Spoon", "Chopsticks", "Fingers"],
     },
     {
       description: "Things found in a bedroom",
+      className: "hard",
       words: ["Pillow", "Pyjama", "Bed", "Lamp"],
     },
     {
       description: "Ones that don't fit the rest",
+      className: "tricky",
       words: ["Shoe", "Ball", "Door", "Canada"],
     },
   ],
--- src/SolvedGroup.jsx
+++ src/SolvedGroup.jsx
@@ -1,8 +1,10 @@
 import classNames from "classnames";
 import "./SolvedGroup.scss";
+
+// eslint-disable-next-line react/prop-types
 const SolvedGroup = ({ solvedGroup }) => (
-  <div className={"solved-group"}>
-    <div>{solvedGroup.description}</div>
+  <div className={classNames("solved-group", solvedGroup.className)}>
+    <div className={"title"}>{solvedGroup.description}</div>
     <div>{solvedGroup.words.join(", ")}</div>
   </div>
 );
