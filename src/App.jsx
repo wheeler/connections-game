c6992ad90abae4a4ae20092e06d2bd0a82411b4d
wheeler <mhwheeler@gmail.com>
2024-04-16 11:49:23 -0700

    Remove Logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Game from "./Game.jsx";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo" alt="React logo" />
-        </a>
-      </div>
       <h1>Connections Dub</h1>
       <div>
         <Game />
