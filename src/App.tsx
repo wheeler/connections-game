16f817aeb28793738032830f31995e3a55e0cd2d
Mark Wheeler <mhwheeler@gmail.com>
2024-04-24 15:26:54 -0700

    naming/footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,23 @@ import { gameData } from "./gameData.ts";
 function App() {
   return (
     <>
-      <h1>Connections Dub</h1>
+      <h1>Connections Game</h1>
       <div>
         <Game gameData={gameData} />
       </div>
-      <p className="foot-disclaimer">written by mark while very rusty</p>
+      <br />
+      <br />
+      <p className="foot-disclaimer">
+        Original game here:{" "}
+        <a href="https://www.nytimes.com/games/connections">
+          NY Times Connections
+        </a>
+        .<br />
+        made by <a href="https://github.com/wheeler">@wheeler</a> while very
+        rusty.
+        <br />
+        Created as an exercise. I claim no ownership.
+      </p>
     </>
   );
 }
